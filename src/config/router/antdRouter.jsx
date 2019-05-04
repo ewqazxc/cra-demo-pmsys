74ca6bd7448b78ef2563529b3b290e01e9b64b99
洪小程 <603550500@qq.com>
2019-05-04 22:49:20 +0800

    准备更新

---
+++ src/config/router/antdRouter.jsx
@@ -0,0 +1,191 @@
+
+import Form from '../../views/antd/form/form/Form';
+import FormZ from '../../views/antd/form/form/FormZ';
+import FormConfig from '../../views/antd/form/formConfig';
+import Table1 from '../../views/antd/table/table1';
+import Table2 from '../../views/antd/table/table2';
+import Table3 from '../../views/antd/table/table3';
+import Table4 from '../../views/antd/table/table4';
+import Table5 from '../../views/antd/table/table5';
+import Table6 from '../../views/antd/table/table6';
+import Table7 from '../../views/antd/table/table7';
+import Table8 from '../../views/antd/table/table8';
+import Table9 from '../../views/antd/table/table9';
+import Table10 from '../../views/antd/table/table10';
+import Table11 from '../../views/antd/table/table11';
+import Table12 from '../../views/antd/table/table12';
+import Table13 from '../../views/antd/table/table13';
+import Table14 from '../../views/antd/table/table14';
+import Table15 from '../../views/antd/table/table15';
+import Table16 from '../../views/antd/table/table16';
+import Table17 from '../../views/antd/table/table17';
+import Table18 from '../../views/antd/table/table18';
+import Table19 from '../../views/antd/table/table19';
+import Table20 from '../../views/antd/table/table20';
+import Table21 from '../../views/antd/table/table21';
+import Table22 from '../../views/antd/table/table22';
+import Table23 from '../../views/antd/table/table23';
+import Table24 from '../../views/antd/table/table24';
+import MyCarousel from '../../views/antd/carousel/carousel';
+import Carousel1 from '../../views/antd/carousel/carousel1';
+import MyCollapse from '../../views/antd/collapse/collapse';
+import MyList from '../../views/antd/list/list';
+
+export const antdRouter = [
+    {
+        path: '/mainAntd/carousel',
+        exact: true,
+        strict: true,
+        component: MyCarousel
+    }, {
+        path: '/mainAntd/carousel1',
+        exact: true,
+        strict: true,
+        component: Carousel1
+    }, {
+        path: '/mainAntd/collapse',
+        exact: true,
+        strict: true,
+        component: MyCollapse
+    }, {
+        path: '/mainAntd/list',
+        exact: true,
+        strict: true,
+        component: MyList
+    }, {
+        path: '/form',
+        exact: true,
+        strict: true,
+        component: Form
+    }, {
+        path: '/formZ',
+        exact: true,
+        strict: true,
+        component: FormZ
+    }, {
+        path: '/formConfig',
+        exact: true,
+        strict: true,
+        component: FormConfig
+    }, {
+        path: '/table1',
+        exact: true,
+        strict: true,
+        component: Table1
+    }, {
+        path: '/table2',
+        exact: true,
+        strict: true,
+        component: Table2
+    }, {
+        path: '/table3',
+        exact: true,
+        strict: true,
+        component: Table3
+    }, {
+        path: '/table4',
+        exact: true,
+        strict: true,
+        component: Table4
+    }, {
+        path: '/table5',
+        exact: true,
+        strict: true,
+        component: Table5
+    }, {
+        path: '/table6',
+        exact: true,
+        strict: true,
+        component: Table6
+    }, {
+        path: '/table7',
+        exact: true,
+        strict: true,
+        component: Table7
+    }, {
+        path: '/table8',
+        exact: true,
+        strict: true,
+        component: Table8
+    }, {
+        path: '/table9',
+        exact: true,
+        strict: true,
+        component: Table9
+    }, {
+        path: '/table10',
+        exact: true,
+        strict: true,
+        component: Table10
+    }, {
+        path: '/table11',
+        exact: true,
+        strict: true,
+        component: Table11
+    }, {
+        path: '/table12',
+        exact: true,
+        strict: true,
+        component: Table12
+    }, {
+        path: '/table13',
+        exact: true,
+        strict: true,
+        component: Table13
+    }, {
+        path: '/table14',
+        exact: true,
+        strict: true,
+        component: Table14
+    }, {
+        path: '/table15',
+        exact: true,
+        strict: true,
+        component: Table15
+    }, {
+        path: '/table16',
+        exact: true,
+        strict: true,
+        component: Table16
+    }, {
+        path: '/table17',
+        exact: true,
+        strict: true,
+        component: Table17
+    }, {
+        path: '/table18',
+        exact: true,
+        strict: true,
+        component: Table18
+    }, {
+        path: '/table19',
+        exact: true,
+        strict: true,
+        component: Table19
+    }, {
+        path: '/table20',
+        exact: true,
+        strict: true,
+        component: Table20
+    }, {
+        path: '/table21',
+        exact: true,
+        strict: true,
+        component: Table21
+    }, {
+        path: '/table22',
+        exact: true,
+        strict: true,
+        component: Table22
+    }, {
+        path: '/table23',
+        exact: true,
+        strict: true,
+        component: Table23
+    }, {
+        path: '/table24',
+        exact: true,
+        strict: true,
+        component: Table24
+    },
+]
